d67db0bc9ef152ba4721014391cade4ac7f46ccf
Iain Houston <mail@iainhouston.me>
2018-01-29 14:20:13 +0000

    Added DPI field to (Councillor) Person

--- scripts/badev/rsyncProd2DevFiles.sh
+++ scripts/badev/rsyncProd2DevFiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rsync -avzn wpbapc:/var/www/drupal/web/sites/default/files/ \
+rsync -avz wpbapc:/var/www/drupal/web/sites/default/files/ \
 $BADEV/web/sites/default/files/ \
 --exclude=js --exclude=php --exclude=css
