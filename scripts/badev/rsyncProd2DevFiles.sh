72dec4879f346ec023723f2ef18e09c3fc222034
Iain <iain@iainhouston.me>
2018-01-30 09:10:31 +0000

    convenience aliases update

--- scripts/badev/rsyncProd2DevFiles.sh
+++ scripts/badev/rsyncProd2DevFiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rsync -avz wpbapc:/var/www/drupal/web/sites/default/files/ \
+rsync -avz $LIVE_SSH_ALIAS:/var/www/drupal/web/sites/default/files/ \
 $BADEV/web/sites/default/files/ \
 --exclude=js --exclude=php --exclude=css
