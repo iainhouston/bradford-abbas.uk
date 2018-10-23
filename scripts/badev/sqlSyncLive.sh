e8386a3b190a829a3dc424bf3dcff7647cb4526d
Iain Houston <mail@iainhouston.me>
2018-10-23 08:32:12 +0100

    Script updates

---
+++ scripts/badev/sqlSyncLive.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Dump remote database to local sql
+NOW=$(date +"%a-%H%M-%d%b%y")
+FILE="$BADEV/vm/saved_sql/live/$NOW.sql"
+echo "Clearing caches before dumping Live database"
+drush @balive cr
+echo "Dumping Live database"
+drush @balive sql:dump > $FILE
+
+# drush uses rsync -t flag which writes extraneous text
+sed -i '' 's/^Connection to/-- Connection to/' $FILE
+
+drush @badev sql:drop -y
+echo "Loading replica of Live database"
+drush @badev sql:cli < $FILE
+drush @badev cr
