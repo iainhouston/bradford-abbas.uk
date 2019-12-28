76e06ee00bb132ea5d34240ec204f719e729f11b
Iain Houston <mail@iainhouston.me>
2019-12-28 19:06:21 +0000

    Media image styles OK

--- scripts/badev/safecex.sh
+++ scripts/badev/safecex.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
-set -x
-
-$DRUSH $DEVALIAS pmu devel kint
+# set -x
+printf '%s\n' "Disabling development modules if they're enabled"
+# : (True) will ignore pmu errors
+: $DRUSH $DEVALIAS pmu devel,kint
+printf '%s\n' "Exporting configurations from running Dev sysytem"
 $DRUSH $DEVALIAS cex -y
