d184fb9f015caadcc82b52c39600a2f06bfccabc
Iain Houston <iainhouston@mac.com>
2020-01-13 14:58:46 +0000

    hand deploy updates. Needs work to automate again.

--- scripts/badev/updateLiveCode.sh
+++ scripts/badev/updateLiveCode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DRUPALVM_ENV=prod \
-ansible-playbook vendor/geerlingguy/drupal-vm/provisioning/playbook.yml \
+ansible-playbook vendor/iainhouston/drupal-vm/provisioning/playbook.yml \
 --inventory-file=vm/inventory \
 --tags=drupal   \
 --extra-vars="config_dir=$(pwd)/vm" \
