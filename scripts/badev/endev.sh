bd67bb26468b7504c0e30983064abce0471b4988
Iain Houston <iainhouston@mac.com>
2020-02-28 10:38:42 +0000

    refactor html emails for LCD mail clients

--- scripts/badev/endev.sh
+++ scripts/badev/endev.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-$DRUSH $DEVALIAS pm-enable devel kint -y
+#$DRUSH $DEVALIAS pm-enable devel kint -y
+$DRUSH $DEVALIAS pm-enable devel -y
 $DRUSH $DEVALIAS cr
 echo "Note: Use of command ${RED}safecex${NC} will uninstall devel modules ...\nbefore exporting configuration to version-controlled ${GREEN}config/sync/*${NC}"
--- scripts/badev/safecex.sh
+++ scripts/badev/safecex.sh
@@ -3,6 +3,7 @@
 # set -x
 printf '%s\n' "Disabling development modules if they're enabled"
 # : (True) will ignore pmu errors
-: $DRUSH $DEVALIAS pmu devel,kint
+#: $DRUSH $DEVALIAS pmu devel,kint
+: $DRUSH $DEVALIAS pmu devel
 printf '%s\n' "Exporting configurations from running Dev sysytem"
 $DRUSH $DEVALIAS cex -y
