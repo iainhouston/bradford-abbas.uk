e832d4b55519ee81ae8ee0d56c8918a4e3499032
Iain Houston <iainhouston@mac.com>
2020-05-19 17:30:57 +0100

    per live updates; clone script update

--- scripts/badev/cloneLive2Dev.sh
+++ scripts/badev/cloneLive2Dev.sh
@@ -6,17 +6,21 @@ FILE="$BADEV/vm/saved_sql/live/$NOW.sql"
 echo "Clearing caches before dumping Live database"
 $DRUSH @balive cr
 echo "Dumping Live database"
-$DRUSH @balive sql:dump > $FILE
+$DRUSH @balive sql:dump --result-file=/var/www/drupal/temp.sql --gzip
+scp $LIVE_SSH_ALIAS:/var/www/drupal/temp.sql.gz $FILE.gz
+gunzip $FILE.gz
 
 # drush uses rsync -t flag which writes extraneous text
-sed -i '' 's/^Connection to/-- Connection to/' $FILE
+# sed -i '' 's/^Connection to/-- Connection to/' $FILE
 
 $DRUSH @badev sql:drop -y
 echo "Loading replica of Live database"
 $DRUSH @badev sql:cli < $FILE
-$DRUSH @badev cr
+# $DRUSH @badev cr
 
 echo "Syncing files from Live to Dev machine"
 rsync -avz $LIVE_SSH_ALIAS:/var/www/drupal/web/sites/default/files/ \
 $BADEV/web/sites/default/files/ \
 --exclude=js --exclude=php --exclude=css
+
+osascript -e 'display notification "Dev Database and Files in sync with Live system" with title "Task complete" sound name "Basso"'
