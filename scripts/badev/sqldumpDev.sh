c78bca14fbb12a2b41631654b3ccf2937d2a55df
Iain Houston <iainhouston@mac.com>
2020-06-27 12:11:55 +0100

    Now running Drupal 9

--- scripts/badev/sqldumpDev.sh
+++ scripts/badev/sqldumpDev.sh
@@ -4,9 +4,9 @@
 NOW=$(date +"%a-%H%M-%d%b%y")
 FILE="$BADEV/vm/saved_sql/dev/$NOW.sql"
 echo "Clearing caches before dumping dev database"
-$DRUSH @badev cr
+$DRUSH $DEVALIAS cr
 echo "Dumping dev database to $FILE"
-$DRUSH @badev sql-dump > $FILE
+$DRUSH $DEVALIAS sql-dump > $FILE
 
 # $DRUSH uses rsync -t flag which writes extraneous text
 sed -i '' 's/^Connection to/-- Connection to/' $FILE
