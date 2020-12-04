ca58f40dcac6d2010dffb840a149a0305651b258
Iain Houston <iainhouston@iains-mac-mini.home>
2020-12-04 16:47:47 +0000

    separate MOTD from cdbadev 2

---
+++ scripts/badev/motd.sh
@@ -0,0 +1,7 @@
+echo "checkVersions  - Review  software required fot development"
+echo "endev          - Enable development modules in Dev site"
+echo "safecex        - Safe export of Dev site's configuration"
+echo "updateLiveCode - Code and Config to Live site"
+echo "${GREEN}drush alias ${RED}${LIVEALIAS}${GREEN} for the Live Server at ${RED}https://${LIVESITE}${NC}"
+echo "${GREEN}drush alias ${RED}${STAGEALIAS}${GREEN} for the Staging Server at ${RED}https://${STAGESITE}${NC}"
+echo "${GREEN}drush alias ${RED}${DEVALIAS}${GREEN} for the Development Server at ${RED}http://${DEVSITE}${NC}"
