819ddff5d10e85771703df62a24cf5b40f0ed0f6
Iain Houston <mail@iainhouston.com>
2022-03-15 20:01:27 +0000

    UpdateLiveCode and provisionLiveServer ready to run
    Neither tested in live situation

--- scripts/badev/provisionLiveServer.sh
+++ scripts/badev/provisionLiveServer.sh
@@ -1,41 +1,51 @@
-#!/bin/zsh
+#!/bin/zsh -x
 
 echo "${RED}Do you really want to do this?"
-echo "${GREEN}library/provisioning/playbook.yml${NC} is expected to contain the most recent Live SQL"
-echo "${GREEN}(Please choose a number in reply)${NC}"
+echo "${GREEN}(Please enter a number now)${NC}"
+
 select choice in "Yes" "No"; do
     case $choice in
-        Yes ) echo "It's a yes. Here goes!"; 
-            echo "You'll need the Administrator's sudo password."
-            echo "This "
+    
+        Yes ) 
+            echo "${GREEN}It's a ${RED}Yes. ${GREEN}Here goes!${NC}"
+            echo " "
+            echo "We'll need the file containig the Administrator's sudo password."
+            echo "This is the same password that automatically unencrypts secrets.yml"
+            echo "We're expecting to find this file here: ${HOME}/.vaultpw"
             break;;
-        No ) exit 3;;
+        No ) 
+            echo "Abandoning execution of this script at your request"            
+            exit 3;;
+            
     esac
 done
 
-Start_time=$SECONDS
-
 if [ -e ${HOME}/.vaultpw ]
+
 then
 
     echo "${GREEN}About to provision ${RED}Live${GREEN} Server{NC}"
+    Start_time=$SECONDS
     
-    cat ${HOME}/.vaultpw
+    becomepass=`cat ${HOME}/.vaultpw`
 
-    # DRUPALVM_ENV=prod ansible-playbook library/provisioning/playbook.yml \
-    #      --inventory-file=prod/inventory \
-    #      --vault-password-file="${HOME}/.vaultpw" \
-    #      --extra-vars="config_dir=${pwd}/vm" \
-    #      --skip-tags=test_only \
-    #          --ask-become-pass
-    
+    DRUPALVM_ENV=prod ansible-playbook library/provisioning/playbook.yml \
+         --inventory-file=prod/inventory \
+         --vault-password-file="${HOME}/.vaultpw" \
+         --extra-vars="config_dir=${pwd}/vm" \
+         --extra-vars="ansible_become_pass=${becomepass}" \
+         --skip-tags=test_only 
+
+    osascript -e 'display notification "Live system provisioned with latest code and configuration" with title "Task complete" sound name "Basso"'    
     Elapsed_time=$(($SECONDS - $Start_time))
-    echo "${GREEN}Update attempt completed in ${RED}${Elapsed_time}${GREEN} seconds${NC}"
+    echo "${GREEN}Provisioning of Live Server completed in ${RED}${Elapsed_time}${GREEN} seconds${NC}"
 
     exit 0
 
 else
+
     echo "${RED}Couldn't find ${GREEN}${HOME}/.vaultpw{NC}"
     echo "Abandoning the provisioning of the Live Server"
-        exit 1
-    fi
+    exit 1
+    
+fi
--- scripts/badev/updateLiveCode.sh
+++ scripts/badev/updateLiveCode.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
-Start_time=$SECONDS
-ansible-playbook prod/update.playbook.yml \
-	--inventory-file=prod/inventory \
-	--extra-vars="project_dir=$(pwd)" \
-	--vault-password-file="~/.vaultpw" \
-	--ask-become-pass
+echo "We'll need the file containig the Administrator's sudo password."
+echo "This is the same password that automatically unencrypts secrets.yml"
+echo "We're expecting to find this file here: ${HOME}/.vaultpw"
 
-osascript -e 'display notification "Live system updated to latest code and configuration" with title "Task complete" sound name "Basso"'
+if [ -e ${HOME}/.vaultpw ]
 
-Elapsed_time=$(($SECONDS - $Start_time))
-echo "${GREEN}Update attempt completed in ${RED}${Elapsed_time}${GREEN} seconds${NC}"
+then
+
+    echo "${GREEN}About to update ${RED}Live${GREEN} Server{NC}"
+    Start_time=$SECONDS
+    
+    becomepass=`cat ${HOME}/.vaultpw`
+
+    ansible-playbook prod/update.playbook.yml \
+        --inventory-file=prod/inventory \
+        --extra-vars="project_dir=$(pwd)" \
+        --extra-vars="ansible_become_pass=${becomepass}" \
+        --vault-password-file="~/.vaultpw" 
+    
+    osascript -e 'display notification "Live system updated to latest code and configuration" with title "Task complete" sound name "Basso"'
+
+    Elapsed_time=$(($SECONDS - $Start_time))
+    echo "${GREEN}Updating of Live Server completed in ${RED}${Elapsed_time}${GREEN} seconds${NC}"
+
+    exit 0
+
+else
+
+    echo "${RED}Couldn't find ${GREEN}${HOME}/.vaultpw{NC}"
+    echo "Abandoning updating the Live Server"
+    exit 1
+    
+fi
+    
