3c529b4d7695ecc9aa8e1aca25f2c9ae87e87c2e
Iain Houston on Leo <iainhouston@mac.com>
2022-06-17 10:10:57 +0100

    Module updates and simplenews_subscriber entity type : The UUID field needs to be updated.

--- scripts/badev/safecex.sh
+++ scripts/badev/safecex.sh
@@ -4,6 +4,6 @@
 printf '%s\n' "Disabling development modules if they're enabled"
 # : (True) will ignore pmu errors
 #: $DRUSH $DEVALIAS pmu devel,kint
-: $DRUSH $DEVALIAS pmu devel
+: $DRUSH $DEVALIAS pm:uninstall devel, devel_entity_updates, devel_kint_extras
 printf '%s\n' "Exporting configurations from running Dev sysytem"
 $DRUSH $DEVALIAS cex -y
