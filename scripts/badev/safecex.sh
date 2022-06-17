2d2f49d6afdf1df3175c14ec1c5c45b0db7f8d7f
Iain Houston on Leo <iainhouston@mac.com>
2022-06-17 10:18:39 +0100

    Correct pm:uninstall in safecex script

--- scripts/badev/safecex.sh
+++ scripts/badev/safecex.sh
@@ -4,6 +4,6 @@
 printf '%s\n' "Disabling development modules if they're enabled"
 # : (True) will ignore pmu errors
 #: $DRUSH $DEVALIAS pmu devel,kint
-: $DRUSH $DEVALIAS pm:uninstall devel, devel_entity_updates, devel_kint_extras
+$DRUSH $DEVALIAS pm:uninstall devel, devel_entity_updates, devel_kint_extras -y
 printf '%s\n' "Exporting configurations from running Dev sysytem"
 $DRUSH $DEVALIAS cex -y
