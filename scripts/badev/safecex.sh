49989f9b53a6a1b9ca42ca5a27a76d1da355e35a
iainhouston <iainhouston@mac.com>
2023-04-12 18:05:07 +0100

    Upgrading iainhouston/pellucid_agenda (V5.0.1 => V5.0.2)

--- scripts/badev/safecex.sh
+++ scripts/badev/safecex.sh
@@ -4,6 +4,7 @@
 printf '%s\n' "Disabling development modules if they're enabled"
 # : (True) will ignore pmu errors
 #: $DRUSH $DEVALIAS pmu devel,kint
-$DRUSH $DEVALIAS pm:uninstall devel, devel_entity_updates, devel_kint_extras -y
+#$DRUSH $DEVALIAS pm:uninstall upgrade_status, devel, devel_entity_updates, devel_kint_extras -y
+$DRUSH $DEVALIAS pm:uninstall upgrade_status, core-dev
 printf '%s\n' "Exporting configurations from running Dev sysytem"
 $DRUSH $DEVALIAS cex -y
